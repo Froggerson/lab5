77cec26cbe992008d2df40ddad11222a9a4fc975
Jennifer <32044772+Froggerson@users.noreply.github.com>
2021-02-04 10:41:06 -0800

    Forgot to add the routes earlier

--- routes/add.js
+++ routes/add.js
@@ -1,5 +1,12 @@
 var data = require("../data.json");
 
-exports.addFriend = function(request, response) {    
-	// Your code goes here
- }
+exports.addFriend = function(request, response) {
+  var name = request.query.name;
+  var description = request.query.description;
+  var newFriend ={
+    "name": name,
+    "description": description,
+    "imageURL": "https://loremflickr.com/400/400/"
+  };
+  data.friends.push(newFriend);
+}
